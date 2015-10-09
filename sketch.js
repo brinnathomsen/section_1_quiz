fbf6121ec3483ce386449e3f2654622bd309176a
brinnathomsen <thomb212@gmail.com>
2015-10-09 16:35:13 -0400

    quiz

--- sketch.js
+++ sketch.js
@@ -5,12 +5,50 @@ function setup() {
 
 function draw() {
 	// clear the background
-	background(50, 50, 50);
+	background(0, 0, 50);
 
 	// set the drawing style
-	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	fill(0, 80, 50);
+	noStroke();
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+	// draw ground
+	rect(0, 250, 640, 110);
+
+	//draw trees
+	for (var i = 0; i < 15; i++) {
+		fill(0, 40, 25);
+		triangle(-50 + i * 50, 250, 0 + i * 50, 180, 50 + i * 50, 250);
+	}
+
+	fill(100, 100, 100);
+	ellipse(mouseX, mouseY, 50, 50);
+	ellipse(mouseX, mouseY + 20, 100, 50);
+
+	fill(50, 50, 50);
+	ellipse(mouseX + 50, mouseY + 20, 10, 10);
+	ellipse(mouseX - 50, mouseY + 20, 10, 10);
+	ellipse(mouseX + 20, mouseY + 20, 10, 10);
+	ellipse(mouseX - 20, mouseY + 20, 10, 10);
+
+	if (!mouseIsPressed) {} else {
+		fill(random(0, 100), random(0, 100), random(0, 100));
+		rect(0, 250, 640, 110);
+
+
+		fill(random(0, 255), random(0, 255), random(0, 255));
+		ellipse(mouseX + 50, mouseY + 20, 10, 10);
+		ellipse(mouseX - 50, mouseY + 20, 10, 10);
+		ellipse(mouseX + 20, mouseY + 20, 10, 10);
+		ellipse(mouseX - 20, mouseY + 20, 10, 10);
+
+
+
+		ellipse(mouseX, 275, 50, 10);
+		for (var i = 0; i < 15; i++) {
+			fill(0, 30, 15);
+			triangle(-50 + i * 50, 250, 0 + i * 50, 180, 50 + i * 50, 250);
+		}
+
+	}
+
 }
